df56a91a13258e44cd336261ed62fba8ad5ea183
tkvashin <timur.kvashin.a@gmail.com>
2020-12-13 13:02:53 +0400

    requirements added

--- main.py
+++ main.py
@@ -3,40 +3,15 @@ from PyQt5 import uic
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
-        self.setStyleSheet("""QScrollBar:vertical {
-            border: 2px solid grey;
-            background: #32CC99;
-            height: 15px;
-            margin: 0px 20px 0 20px;
-        }
-        QScrollBar::handle:vertical {
-            background: white;
-            min-width: 20px;
-        }
-        QScrollBar::add-line:vertical {
-            border: 2px solid grey;
-            background: #32CC99;
-            width: 20px;
-            subcontrol-position: right;
-            subcontrol-origin: margin;
-        }
-
-        QScrollBar::sub-line:vertical {
-            border: 2px solid grey;
-            background: #32CC99;
-            width: 20px;
-            subcontrol-position: left;
-            subcontrol-origin: margin;
-        }
-                                      }""")
+        self.setFixedSize(442, 538)
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = MyWidget()
+    ex = MainWindow()
     ex.show()
     sys.exit(app.exec())
