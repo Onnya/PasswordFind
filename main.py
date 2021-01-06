43b39c4f06c99f29526a116b816fbcef9523d276
tkvashin <timur.kvashin.a@gmail.com>
2021-01-06 17:16:34 +0300

    Some cosmetic improvements

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import sys
+
 import subprocess
-from PyQt5 import uic
-from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-from addeditwindow import AddEditWindow
-import sqlite3
+
 import os.path
+
+from datetime import datetime
+
+import sqlite3
+
+import csv
+
+from PyQt5 import uic, QtGui, QtCore
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
+
+from addeditwindow import AddWindow, EditWindow
+
 from passwordtools import make_key, take_pr_key, take_pub_key, encrypt_pswrd, decrypt_pswrd
 
 
@@ -12,78 +22,85 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('main.ui', self)
+
         self.setFixedSize(442, 538)
-        self.con = sqlite3.connect("passwords.sqlite")
+        self.setWindowIcon(QtGui.QIcon('img.png'))
+
         self.addBtn.clicked.connect(self.show_add_win)
         self.changeBtn.clicked.connect(self.show_edit_win)
         self.showBtn.clicked.connect(self.show_hide)
         self.deleteBtn.clicked.connect(self.delete)
-        self.send = ""
-        self.update_list()
         self.copyBtn.clicked.connect(self.to_clip)
+        self.exportBtn.clicked.connect(self.export)
+
+        self.con = sqlite3.connect("passwords.sqlite")
         if not os.path.isfile('public.pem'):
             make_key()
 
+        self.AddWin = AddWindow(self)
+        self.EditWin = EditWindow(self)
+
+        self.AddWin.okBtn.clicked.connect(self.update_list)
+        self.EditWin.okBtn.clicked.connect(self.update_list)
+
+        self.update_list()
+
     def show_edit_win(self):
-        if self.listWidget.selectedItems() != []:
-            self.editWin = AddEditWindow(self)
+        if self.listWidget.selectedItems():
             self.statusBar().clearMessage()
-            self.editWin.nameLn.setText(self.listWidget.currentItem().text())
-            self.editWin.move(self.x() + 50, self.y() + 100)
-            self.send = "Change"
-            self.editWin.okBtn.clicked.connect(self.ok_pressed)
-            self.editWin.nameLn.setReadOnly(True)
-            self.editWin.show()
+            self.EditWin.refresh(self.x(), self.y(), self.listWidget.currentItem().text())
+            self.EditWin.show()
         else:
             self.statusBar().showMessage("Name not selected", 2000)
 
     def show_add_win(self):
-        self.editWin = AddEditWindow(self)
-        self.editWin.move(self.x() + 50, self.y() + 100)
-        self.send = "Add"
-        self.editWin.okBtn.clicked.connect(self.ok_pressed)
-        self.editWin.show()
-
-    def ok_pressed(self):
-        self.editWin.check_close(self.send)
-        self.update_list()
+        self.AddWin.refresh(self.x(), self.y())
+        self.AddWin.show()
 
     def update_list(self):
         self.listWidget.clear()
+
         cur = self.con.cursor()
         result = cur.execute("SELECT name FROM main").fetchall()
         result.sort(key=lambda x: str(x))
+
         for i in result:
             self.listWidget.addItem(str(i[0]))
 
     def to_clip(self):
-        if self.listWidget.selectedItems() != []:
+        if self.listWidget.selectedItems():
             name = self.listWidget.currentItem().text()
+
             cur = self.con.cursor()
             result = cur.execute("SELECT password FROM main WHERE name=?",
                                  (name,)).fetchall()
+
             subprocess.Popen(['clip'], stdin=subprocess.PIPE).communicate(
                 bytes(decrypt_pswrd(result[0][0], take_pr_key()), encoding='utf8'))
         else:
             self.statusBar().showMessage("Name not selected", 2000)
 
     def show_hide(self):
-        if self.listWidget.selectedItems() != []:
+        if self.listWidget.selectedItems():
             name = self.listWidget.currentItem().text()
+
             cur = self.con.cursor()
             result = cur.execute("SELECT password FROM main WHERE name=?",
                                  (name,)).fetchall()
+
             self.statusBar().showMessage(decrypt_pswrd(result[0][0], take_pr_key()), 3000)
         else:
             self.statusBar().showMessage("Name not selected", 2000)
 
     def delete(self):
-        if self.listWidget.selectedItems() != []:
+        if self.listWidget.selectedItems():
             self.statusBar().clearMessage()
+
             name = self.listWidget.currentItem().text()
             cur = self.con.cursor()
             cur.execute("DELETE FROM main WHERE name=?", (name,))
             self.con.commit()
+
             self.update_list()
         else:
             self.statusBar().showMessage("Name not selected", 2000)
@@ -102,6 +119,5 @@ if __name__ == '__main__':
     ex.show()
     sys.exit(app.exec())
 
-# TODO: шифровка change
 # TODO: импорт экспорт
 # TODO: загрузка с паролем (?)
